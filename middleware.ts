9c87f98001c253305cf8c09fbb06db7f52a256a2
Carson Yang <yangchuansheng33@gmail.com>
2025-04-12 15:15:06 +0800

    feature: add llms.txt (#76)
    Signed-off-by: yangchuansheng <yangchuansheng33@gmail.com>

--- middleware.ts
+++ middleware.ts
@@ -24,6 +24,6 @@ export async function middleware(request: NextRequest, event: NextFetchEvent) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|images/|icons/|favicon/|favicon.ico|logo.svg|sitemap.xml).*)/',
+    '/((?!api|_next/static|_next/image|images/|icons/|favicon/|favicon.ico|logo.svg|sitemap.xml|llms.txt).*)/',
   ],
 };
